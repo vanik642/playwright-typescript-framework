facd90afd69a223b02ef994a89f108795c478fc3
Vanita K <vkanaje@gmail.com>
2026-01-22 16:20:09 -0800

    lint error Fixed

--- fixtures/baseFixtures.ts
+++ fixtures/baseFixtures.ts
@@ -1,6 +1,6 @@
 import {test as base,expect} from '@playwright/test';
-import {HomePage} from '../pages/HomePage.ts';
-import {LoginPage} from '../pages/LoginPage.ts'
+import {HomePage} from '../pages/HomePage';
+import {LoginPage} from '../pages/LoginPage';
 
 
 type myFixtures={
@@ -13,19 +13,19 @@ export const test=base.extend<myFixtures>({
 
         //login auromatically before my test
         const loginPage=new LoginPage(page);
-        await loginPage.goToLoginPage(baseURL)
+        await loginPage.goToLoginPage(baseURL);
 
         const username=testInfo.project.metadata.appUsername;
         const password=testInfo.project.metadata.appPassword;
 
         const homePage=await loginPage.doLogin(username,password);
         expect(await homePage.isUserLoggedIn()).toBeTruthy();
-        await use(homePage)
+        await use(homePage);
 
         
     }
 
-})
+});
 
 
 export {expect };
